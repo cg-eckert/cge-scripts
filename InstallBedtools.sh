687924879e23caab0333af16be2c7275adc9d463
Chris Eckert <chris.eckert@queensu.ca>
2013-07-02 17:02:45 -0400

    Update InstallBedtools.sh

--- InstallBedtools.sh
+++ InstallBedtools.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /mnt
+cd /storage/chris.eckert
 wget https://bedtools.googlecode.com/files/BEDTools.v2.17.0.tar.gz
 tar zxvf BEDTools.v2.17.0.tar.gz
 cd bedtools-2.17.0/
 make
-cp bin/bedtools /usr/local/bin/
-cd /mnt
+cp bin/bedtools /storage/chris.eckert/bin/
+cd /storage/chris.eckert
